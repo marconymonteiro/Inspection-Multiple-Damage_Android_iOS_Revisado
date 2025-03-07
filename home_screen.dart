d24feba2f6bb062fc221ab9eb9a80ce33a0dddbf
marconymonteiro <65182991+marconymonteiro@users.noreply.github.com>
2025-03-07 14:27:56 -0300

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'inspection_form.dart'; // Importa a tela de novo formulário
-import 'saved_forms.dart';    // Importa a tela de formulários salvos
+import 'saved_forms.dart'; // Importa a tela de formulários salvos
 import 'package:uuid/uuid.dart';
 import 'local_storage.dart'; // Importa o arquivo onde você implementou checkAndSendPendingForms()
 
+// Função para gerar um ID único para o formulário
 String generateUniqueFormId() {
   var uuid = Uuid();
   return uuid.v4();
@@ -15,10 +16,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _isLoading = false;
+
   @override
   void initState() {
     super.initState();
-    checkAndSendPendingForms(); // Verifica e envia formulários pendentes ao iniciar
+    _checkAndSyncPendingForms(); // Verifica e envia formulários pendentes ao iniciar
+  }
+
+  // Função para verificar e sincronizar formulários pendentes
+  Future<void> _checkAndSyncPendingForms() async {
+    setState(() {
+      _isLoading = true; // Ativa o estado de carregamento
+    });
+
+    try {
+      await checkAndSendPendingForms(); // Chama a função para sincronizar formulários pendentes
+    } catch (e) {
+      print('Erro ao sincronizar formulários pendentes: $e');
+    } finally {
+      setState(() {
+        _isLoading = false; // Desativa o estado de carregamento
+      });
+    }
   }
 
   @override
@@ -27,44 +47,61 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Tela Inicial'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Logo do cliente - Imagem acima dos botões
-            Image.asset(
-              'assets/logo_cliente.png', // Caminho para a logo do cliente
-              width: 150, // Ajuste o tamanho conforme necessário
-              height: 150,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo do cliente - Imagem acima dos botões
+                Image.asset(
+                  'assets/logo_cliente.png', // Caminho para a logo do cliente
+                  width: 150, // Ajuste o tamanho conforme necessário
+                  height: 150,
+                ),
+                SizedBox(height: 30), // Espaçamento entre a logo e os botões
+
+                // Botão para iniciar um novo formulário
+                ElevatedButton(
+                  onPressed: () {
+                    final formId = generateUniqueFormId(); // Gera um ID único para o formulário
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FormularioInspecao(formId: formId),
+                      ),
+                    );
+                  },
+                  child: Text('Novo Formulário'),
+                ),
+                SizedBox(height: 20),
+
+                // Botão para consultar formulários salvos
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SavedForms(), // Navega para a tela de formulários salvos
+                      ),
+                    );
+                  },
+                  child: Text('Consultar Formulários'),
+                ),
+              ],
             ),
-            SizedBox(height: 30), // Espaçamento entre a logo e os botões
-            // Botão para iniciar um novo formulário
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FormularioInspecao(formId: generateUniqueFormId()),
-                  ),
-                );
-              },
-              child: Text('Novo Formulário'),
+          ),
+          // Tela de carregamento enquanto sincroniza formulários pendentes
+          Visibility(
+            visible: _isLoading,
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
             ),
-            SizedBox(height: 20),
-            // Botão para consultar formulários salvos
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SavedForms(), // Navega para a tela de formulários salvos
-                  ),
-                );
-              },
-              child: Text('Consultar Formulários'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
